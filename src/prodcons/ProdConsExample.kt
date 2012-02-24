46d0f8bc9c95a182598b0162704a3af5713d92e4
Sergey Lukjanov <me@frostman.ru>
2012-02-24 15:32:03 +0400

    prodcons fixed

--- src/prodcons/ProdConsExample.kt
+++ src/prodcons/ProdConsExample.kt
@@ -2,15 +2,21 @@ package prodcons
 
 import java.util.concurrent.*
 import java.util.concurrent.atomic.AtomicInteger
+import java.io.PrintWriter
 
-fun main(args : Array<String>) = ProdConsExample(100, 1, 10).demo()
+fun main(args : Array<String>) = ProdConsExample(1000000, 20, 10).demo()
 
 public class ProdConsExample(val resourcesCount : Int, val producers : Int, val consumers : Int) {
+    private val nextResId = AtomicInteger(1)
+    private val output = PrintWriter(System.out)
+
     private val resources = LinkedBlockingQueue<Resource?>()
+    private val produced = AtomicInteger(0)
     private val notProduced = CountDownLatch(resourcesCount)
     private val notConsumed = CountDownLatch(resourcesCount)
 
     public fun demo() {
+        val start = System.currentTimeMillis()
         println("Starting prodcons demo with $resourcesCount resources")
         for (i in 1..consumers) Consumer(i).start()
         for (j in 1..producers) Producer(j).start()
@@ -21,11 +27,9 @@ public class ProdConsExample(val resourcesCount : Int, val producers : Int, val 
         println("All resources produced")
 
         notConsumed.await()
-        println("All resources consumed, end of demo.")
-    }
+        println("All resources consumed, demo takes about ${System.currentTimeMillis() - start}ms.")
 
-    class object {
-        private val NEXT_RES_ID = AtomicInteger(1)
+        output.close()
     }
 
     public class Producer(val id : Int) : Runnable {
@@ -34,7 +38,7 @@ public class ProdConsExample(val resourcesCount : Int, val producers : Int, val 
         public fun start() : Unit = thread.start()
 
         override public fun run() {
-            while (notProduced.getCount() > 0) {
+            while (notProduced.getCount() > 0 && produced.incrementAndGet() <= resourcesCount) {
                 val resource = Resource()
                 resources.put(resource)
                 resource.produced(id)
@@ -59,9 +63,11 @@ public class ProdConsExample(val resourcesCount : Int, val producers : Int, val 
     }
 
     public class Resource() {
-        private val id : Int = NEXT_RES_ID.getAndIncrement().sure()
+        private val id : Int = nextResId.getAndIncrement()
 
         public fun produced(producerId : Int) : Unit = println("Produced: #$id by #$producerId")
         public fun consumed(consumerId : Int) : Unit = println("Consumed: #$id by #$consumerId")
     }
+
+    fun println(str : String) = output.println("$str")
 }
